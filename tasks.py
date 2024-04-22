ce86f473342350e0b5b7afff4afe723d5a64ecfe
Manu <manuelrey51@gmail.com>
2024-04-22 13:49:45 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -45,6 +45,7 @@ class FoxNewsSearch:
         options.add_argument("--disable-dev-shm-usage")  
         options.add_argument("--headless") 
         options.add_argument("--remote-debugging-port=9222")
+        
         self.driver = webdriver.Chrome(options=options)
         self.wait = WebDriverWait(self.driver, 10)
     
