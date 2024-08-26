1c1c7b2380418bd99d5ad6607767e9326f341414
Manu <157758612+ManuMorales777@users.noreply.github.com>
2024-08-26 01:20:09 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -11,9 +11,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys,ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from robocorp.tasks import task
 from robocorp import workitems, browser, vault
 
