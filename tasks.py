e536f40cc45f24c2b4da5501386b2dc3161a7490
Manu <157758612+ManuMorales777@users.noreply.github.com>
2024-08-26 00:51:14 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from openpyxl import Workbook
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
