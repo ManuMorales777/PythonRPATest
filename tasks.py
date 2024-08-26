425e4cb52401d9618c0b78562830bfb19c911d6d
Manu <157758612+ManuMorales777@users.noreply.github.com>
2024-08-26 14:48:20 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -150,7 +150,7 @@ def minimal_task():
     #chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     #chrome_options.add_argument("--disable-dev-shm-usage")
-    #chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     #chrome_options.add_experimental_option("prefs", chrome_prefs)
     browser = webdriver.Chrome(options=chrome_options)
 
